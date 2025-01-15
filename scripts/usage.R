068811e920ed07c2b42041b9d78ba98dfdb71c70
tom.hunter <tom.hunter@cityandguilds.com>
2025-01-15 09:52:16 +0000

    initial commit with project set up

---
+++ scripts/usage.R
@@ -0,0 +1,65 @@
+# Libraries
+library(tidyverse)
+library(here)
+
+
+# read in data: pokemon.csv accessible here:
+# https://www.kaggle.com/datasets/rounakbanik/pokemon
+pokedata <- read.csv(here("data","pokemon.csv"))
+
+
+########################## FUNCTION 1 ##########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################## FUNCTION 2 ##########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################## FUNCTION 3 ##########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################## FUNCTION 4 ##########################
